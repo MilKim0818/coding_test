cadf30fe6507d66343b2f336bf00c2ec00d3e3b0
Miri Kim <124236172+MilKim0818@users.noreply.github.com>
2023-03-31 16:44:26 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,7 @@
+#def solution(emergency):
+#    answer = []
+#    return answer
+
+def solution(emergency):
+    응급순서 = sorted(emergency, reverse=True)
+    return [응급순서.index(응급도) + 1 for 응급도 in emergency]
